640ffd925cd1a739ecd3702fd97f44aebfdf1a06
Grant Ours <grant.ours55@gmail.com>
2022-09-02 13:56:57 -0600

    commit tree 11571c99812341989ab63d59c7ab7c5f796f0872

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex) VALUES ('Greg', 'M');
+INSERT INTO bears (name, sex) VALUES ('Greg2', 'M');
+INSERT INTO bears (name, sex) VALUES ('Greg3', 'M');
+INSERT INTO bears (name, sex) VALUES ('Greg4', 'M');
+INSERT INTO bears (name, sex) VALUES ('Greg5', 'M');
+INSERT INTO bears (name, sex) VALUES ('Greg6', 'M');
+INSERT INTO bears (name, sex) VALUES ('Greg7', 'M');
+INSERT INTO bears (name, sex) VALUES (NULL, 'M');
